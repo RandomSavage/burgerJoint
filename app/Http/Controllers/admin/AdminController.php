29fbd30cfea275851859548cfc42aabb406f881e
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2020-03-31 19:48:33 -0400

    Creating Graphs for Dashboard

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
+use App\Member;
+use App\Reservation;
+use DB;
+
+
 
 class AdminController extends Controller {
 
@@ -12,6 +18,61 @@ class AdminController extends Controller {
   }
 
   public function dashboard() {
-    return view('admin/dashboard');
+    $estimated_income_last_30 = DB::select(DB::raw('
+      SELECT
+        (sum(guests_total) * 27) as total
+      FROM restaurant.reservations
+      WHERE created_at BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()
+    '));
+
+    $total_customers_last_30 = DB::select(DB::raw('
+      SELECT
+        sum(guests_total) as total
+      FROM restaurant.reservations
+      WHERE created_at BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()
+    '));
+
+    $total_reservations_last_30 = DB::select(DB::raw('
+      SELECT
+        count(*) as total
+      FROM restaurant.reservations
+      WHERE created_at BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()
+    '));
+
+    $total_employees = DB::select(DB::raw('
+      SELECT
+        count(*) as total
+        FROM users
+        INNER JOIN role_user ON role_user.user_id = user.id
+        INNER JOIN roles ON roles.id = role_user.role_id
+        WHERE role_id =2
+    '));
+
+    return view('admin/dashboard', [
+      "estimated_income_last_30" => "$".$estimated_income_last_30[0]->total,
+      "total_customers_last_30" => $total_customers_last_30[0]->total,
+      "total_reservations_last_30" => $total_reservations_last_30[0]->total,
+      "total_employees" => $total_employees[0]->total,
+    ]);
+  }
+
+  public function dailyRevenueLast30() {
+    // return $estimated_income_daily_data = DB::select(DB::raw('
+    //   SELECT
+    //     DATE_FORMAT(created_at, %Y-%m-%d) as reserved_day,
+    //     (sum(guests_total) * 27) as estimated_earnings,
+    //     count(*) as total_reservations,
+    //     sum(guests_total)
+    //   FROM restaurant.reservations
+    //   GROUP BY reserved_day desc;
+    // '));
+    return $estimated_income_daily_data = DB::select(DB::raw('
+      SELECT
+          DATE_FORMAT(created_at,"%Y-%m-%d") as x,
+          (sum(guests_total) * 27 ) as y
+      FROM reservations
+      GROUP BY x desc;
+    '));
+
   }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -31,61 +31,72 @@
                 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="text-muted">Total Revenue</h5>
+                            <h5 class="text-muted">Estimated Revenue</h5>
                             <div class="metric-value d-inline-block">
-                                <h1 class="mb-1">$12099</h1>
+                                <h1 class="mb-1">{{$estimated_income_last_30}}</h1>
                             </div>
                             <div class="metric-label d-inline-block float-right text-success font-weight-bold">
                                 <span><i class="fa fa-fw fa-arrow-up"></i></span><span>5.86%</span>
                             </div>
                         </div>
-                        <div id="sparkline-revenue"></div>
+                        <!-- <div id="sparkline-revenue"></div> -->
                     </div>
                 </div>
                 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="text-muted">Affiliate Revenue</h5>
+                            <h5 class="text-muted">Total Customers</h5>
                             <div class="metric-value d-inline-block">
-                                <h1 class="mb-1">$12099</h1>
+                                <h1 class="mb-1">{{$total_customers_last_30}}</h1>
                             </div>
                             <div class="metric-label d-inline-block float-right text-success font-weight-bold">
                                 <span><i class="fa fa-fw fa-arrow-up"></i></span><span>5.86%</span>
                             </div>
                         </div>
-                        <div id="sparkline-revenue2"></div>
+                        <!-- <div id="sparkline-revenue2"></div> -->
                     </div>
                 </div>
                 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="text-muted">Refunds</h5>
+                            <h5 class="text-muted">Total Reservations</h5>
                             <div class="metric-value d-inline-block">
-                                <h1 class="mb-1">0.00</h1>
+                                <h1 class="mb-1">{{$total_reservations_last_30}}</h1>
                             </div>
                             <div class="metric-label d-inline-block float-right text-primary font-weight-bold">
                                 <span>N/A</span>
                             </div>
                         </div>
-                        <div id="sparkline-revenue3"></div>
+                        <!-- <div id="sparkline-revenue3"></div> -->
                     </div>
                 </div>
                 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="text-muted">Avg. Revenue Per User</h5>
+                            <h5 class="text-muted">Total Employees</h5>
                             <div class="metric-value d-inline-block">
-                                <h1 class="mb-1">$28000</h1>
+                                <h1 class="mb-1">{{$total_employees}}</h1>
                             </div>
                             <div class="metric-label d-inline-block float-right text-secondary font-weight-bold">
                                 <span>-2.00%</span>
                             </div>
                         </div>
-                        <div id="sparkline-revenue4"></div>
+                        <!-- <div id="sparkline-revenue4"></div> -->
                     </div>
                 </div>
             </div>
             <div class="row">
+              <div class="col-sm-12 col-12">
+                  <div class="card">
+                      <h5 class="card-header">Estimated Daily Revenue</h5>
+                      <div class="card-body">
+                          <div id="morris_totalrevenue"></div>
+                      </div>
+                      <div class="card-footer">
+                          <p class="display-7 font-weight-bold"><span class="text-primary d-inline-block">Estimated Revenue Last 30 Days: {{$estimated_income_last_30}}</span><span class="text-success float-right">+9.45%</span></p>
+                      </div>
+                  </div>
+              </div>
                 <!-- ============================================================== -->
 
                 <!-- ============================================================== -->
@@ -416,18 +427,6 @@
                 <!-- ============================================================== -->
                 <!-- end category revenue  -->
                 <!-- ============================================================== -->
-
-                <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-12">
-                    <div class="card">
-                        <h5 class="card-header"> Total Revenue</h5>
-                        <div class="card-body">
-                            <div id="morris_totalrevenue"></div>
-                        </div>
-                        <div class="card-footer">
-                            <p class="display-7 font-weight-bold"><span class="text-primary d-inline-block">$26,000</span><span class="text-success float-right">+9.45%</span></p>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div class="row">
                 <div class="col-xl-5 col-lg-6 col-md-6 col-sm-12 col-12">
