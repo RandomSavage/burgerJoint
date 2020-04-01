101177719a0a305b8d4ff64da05770b774843969
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2020-04-01 16:05:49 -0400

    adding items to Food Menu

--- resources/views/menu/single-menu.blade.php
+++ resources/views/menu/single-menu.blade.php
@@ -13,7 +13,7 @@
         <div class="content-box">
           <div class="row">
             <div class="col-md-12">
-              <h1>Burgers</h1>
+              <h1>{{$foodItem}}</h1>
             </div>
             <div class="col-md-6">
               <div class="item">
@@ -26,39 +26,19 @@
                 </div>
               </div>
             </div>
-            <div class="col-md-6">
-              <div class="item">
-                <div class="title">
-                  <h4>Texas Burger</h4>
-                  <span class="price">$9</span>
-                </div>
-                <div class="description">
-                  <p>Fresh Slain Steer Ground Sirloin smothered in BBQ Sauce</p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="item">
-                <div class="title">
-                  <h4>Texas Burger</h4>
-                  <span class="price">$9</span>
-                </div>
-                <div class="description">
-                  <p>Fresh Slain Steer Ground Sirloin smothered in BBQ Sauce</p>
+            @foreach ($foodItems as $item)
+              <div class="col-md-6">
+                <div class="item">
+                  <div class="title">
+                    <h4>{{$item->title}}</h4>
+                    <span class="price">{{$item->price}}</span>
+                  </div>
+                  <div class="description">
+                    <p>{{$item->description}}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="col-md-6">
-              <div class="item">
-                <div class="title">
-                  <h4>Texas Burger</h4>
-                  <span class="price">$9</span>
-                </div>
-                <div class="description">
-                  <p>Fresh Slain Steer Ground Sirloin smothered in BBQ Sauce</p>
-                </div>
-              </div>
-            </div>
+            @endforeach
           </div>
         </div>
       </div>
